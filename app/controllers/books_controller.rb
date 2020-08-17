cefb3a93653aa37f7d11c76fbfd23f4a5ddf3658
ksk <ksk@ksknoMacBook-Pro.local>
2020-08-17 11:34:11 +0900

    Update 4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,8 +31,7 @@ class BooksController < ApplicationController
 		flash[:notice] = "Book was successfully updated."
 		redirect_to book_path(@book.id)
 	else
-    	@books = Book.all
-    	render :index
+      render :edit
     end
   end
 
